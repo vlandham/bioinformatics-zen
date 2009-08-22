c9a39c75ef84b313b67096747e6a7975cca452c0
Michael Barton <mail@michaelbarton.me.uk>
2009-08-22 22:15:11 +0100

    Updated compass config

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ project_type = :stand_alone
 http_path = "/"
 sass_dir = "_css"
 css_dir = "_site"
-images_dir = "images"
+images_dir = "_images"
 output_style = :compact
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
